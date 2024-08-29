cce63311bdbb4e2bbf4250304b9ed5df3d579644
Viachaslau Tyshkavets <pmstss@gmail.com>
2024-08-29 10:40:50 +0400

    test(client): fix cypress config and restore e2e tests functionality

--- client/cypress/support/e2e.ts
+++ client/cypress/support/e2e.ts
@@ -1,5 +1,5 @@
 // ***********************************************************
-// This example support/index.js is processed and
+// This example support/e2e.ts is processed and
 // loaded automatically before your test files.
 //
 // This is a great place to put global configuration and
@@ -15,3 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
+
+Cypress.on('uncaught:exception', () => {
+  // Returning false here prevents Cypress from failing the test
+  return false;
+});
